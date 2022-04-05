1279975aa5206acf8a881d9cfa32fdda0dd8aa64
Dyrden <mark771e@stud.kea.dk>
2022-04-05 08:37:50 +0200

    Added | Adventure.java - Small content

---
+++ src/com/company/Adventure.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Adventure {
+
+    private Player player;
+
+    public Adventure() {
+        this.player = new Player();
+
+    }
+
+
+    private void test(){
+        //run something
+    }
+
+    public void run() {
+
+
+    }
+
+
+
+
+}
